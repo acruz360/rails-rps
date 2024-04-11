a3fe6ee80f22d4eab0062619d0866fb03d51057e
acruz360 <158589859+acruz360@users.noreply.github.com>
2024-04-11 14:15:26 +0000

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "game", :action => "rules"})
 
+  get("/rock", { :controller => "game", :action => "rock"})
+
+  get("/paper", { :controller => "game", :action => "paper"})
+
+  get("/scissors", { :controller => "game", :action => "scissors"})
 end
+
